efe1746f33aca5a37a79aa5a9669f8ec2df7828a
Horace Lee <horaceleedev@gmail.com>
2025-11-18 13:50:34 +0000

    Incorporate VideoLinkButton, IndividualLinkButton, and UserLabel into group labels - TODO fix link template later

--- src/components/withSortingAndGrouping.tsx
+++ src/components/withSortingAndGrouping.tsx
@@ -1,11 +1,16 @@
 import React, { useMemo } from "react";
 import { RecordModel } from "pocketbase";
 import { groupBy, orderBy } from "es-toolkit";
-import { Collapse } from "antd";
+import { Collapse, Space } from "antd";
 
-import { MetadataFieldsType } from "../types";
+import { IndividualLinkButton, UserLabel, VideoLinkButton } from "./smart-components/LinkButtons";
+import { MetadataFieldsType, RecordType } from "../types";
 
-type OuterProps<T> = {
+interface BasicGridViewProps {
+  openModal?: (type: RecordType, id: string) => void
+};
+
+interface OuterProps<T> {
   processedRecords: T[];
   metadataFields: MetadataFieldsType;
   processedRecordsPropName: string; // e.g. 'individuals', 'videos', 'crops'
@@ -15,7 +20,7 @@ type OuterProps<T> = {
   groupOrders: ("asc" | "desc")[];
 };
 
-const withSortingAndGrouping = <P extends object, T extends RecordModel>(
+const withSortingAndGrouping = <P extends BasicGridViewProps, T extends RecordModel>(
   BasicGridView: React.ComponentType<P>
 ) => {
   return ({
@@ -46,33 +51,71 @@ const withSortingAndGrouping = <P extends object, T extends RecordModel>(
     if (groupFields.length === 0) {
       return <BasicGridView {...basicGridViewProps} {...{[processedRecordsPropName]: recordsSorted}} />;
     }
-    return groupedRecords.map(([groupValue, groupRecords]) => (
-      <Collapse
-        key={groupValue}
-        collapsible="header"
-        defaultActiveKey={['1']}
-        style={{
-          marginBottom: 12
-        }}
-        items={[
-          {
-            key: '1',
-            label:  (
-              (metadataFields[groupFields[0]].displayBooleanValuesAs) ? 
-              // Use 'displayBooleanValuesAs'
-              <span>{metadataFields[groupFields[0]].displayBooleanValuesAs[Number(groupValue === 'true')]}</span>
-              :
-              <span>{metadataFields[groupFields[0]].displayName}: {groupValue}</span>
-            ),
-            children: (
-              <BasicGridView {...basicGridViewProps} {...{[processedRecordsPropName]: groupRecords}} />
-            ),
-          },
-        ]}
-        // expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-        // style={{ background: token.colorBgContainer }}
-      />
-    ));
+    return groupedRecords.map(([groupValue, groupRecords]) => {
+      let renderedGroupValue = groupValue;
+      const renderType = metadataFields[groupFields[0]].renderType;
+      if (renderType === 'video_link') {
+        renderedGroupValue = (
+          <VideoLinkButton
+            id={groupValue as string}
+            // TODO pass linkTemplate in
+            // linkTemplate={videoLinkTemplate}
+            openModal={basicGridViewProps.openModal}
+          />
+        );
+      } else if (renderType === 'individual_link') {
+        renderedGroupValue = (
+          <IndividualLinkButton
+            id={groupValue as string}
+            // TODO pass linkTemplate in
+            // linkTemplate={individualLinkTemplate}
+            openModal={basicGridViewProps.openModal}
+          />
+        );
+      } else if (renderType === 'user_label') {
+        if (metadataFields[groupFields[0]].valueEditorType === 'multiselect') {
+          const userIds: string[] = groupRecords[0][groupFields[0]];
+          renderedGroupValue = userIds.map(val => (
+            <UserLabel id={val} />
+          ));
+        } else {
+          renderedGroupValue = <UserLabel id={groupValue as string} />;
+        }
+      }
+
+      return (
+        <Collapse
+          key={groupValue}
+          collapsible="header"
+          defaultActiveKey={['1']}
+          style={{
+            marginBottom: 12
+          }}
+          items={[
+            {
+              key: '1',
+              label:  (
+                (metadataFields[groupFields[0]].displayBooleanValuesAs) ? 
+                // Use 'displayBooleanValuesAs'
+                <span>{metadataFields[groupFields[0]].displayBooleanValuesAs[Number(groupValue === 'true')]}</span>
+                :
+                // the <Space> helps with rendering multiple <UserLabel>'s, and
+                // preventing <VideoLinkButton> and <IndividualLinkButton> from taking up the full width
+                <Space>
+                  <span>{metadataFields[groupFields[0]].displayName}:</span>
+                  {renderedGroupValue}
+                </Space>
+              ),
+              children: (
+                <BasicGridView {...basicGridViewProps} {...{[processedRecordsPropName]: groupRecords}} />
+              ),
+            },
+          ]}
+          // expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+          // style={{ background: token.colorBgContainer }}
+        />
+      );
+    });
   };
 };
 
