53b9111308c7117ed1a97449c3524a481386e074
Horace Lee <horaceleedev@gmail.com>
2025-11-19 16:19:18 +0000

    Update DashboardContent to accept `style` prop

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Layout } from "antd";
 const { Content } = Layout;
 
-const DashboardContent: React.FC<{children: React.ReactNode}> = ({children}) => {
+const DashboardContent: React.FC<{children: React.ReactNode, style?: React.CSSProperties}> = ({children, style}) => {
   return (
-    <Content style={{padding: "36px 48px 48px 48px"}}>
+    <Content style={{padding: "36px 48px 48px 48px", ...style}}>
       {children}
     </Content>
   )
