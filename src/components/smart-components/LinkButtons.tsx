8bc1f4eef98d97984f3a840879c0fb31d4faf9a9
Timothy Poon <62692924+ptim0626@users.noreply.github.com>
2026-01-20 13:36:37 +0000

    Extend LinkButtonProps so that timestamp can be appended to video URL

--- src/components/smart-components/LinkButtons.tsx
+++ src/components/smart-components/LinkButtons.tsx
@@ -11,6 +11,10 @@ interface LinkButtonProps {
   openModal?: (type: RecordType, id: string) => void;
 };
 
+interface VideoLinkButtonProps extends LinkButtonProps {
+  timestamp?: number;
+}
+
 export const IndividualLinkButton: React.FC<LinkButtonProps> = ({
   id,
   linkTemplate = "/individuals/:individualId",
@@ -49,14 +53,21 @@ export const IndividualLinkButton: React.FC<LinkButtonProps> = ({
 export const VideoLinkButton: React.FC<LinkButtonProps> = ({
   id,
   linkTemplate = "/videos/:videoId",
+  timestamp,
   openModal,
 }: LinkButtonProps) => {
   const video = useVideoStore((state) => state.processedRecords.find(v => v.id === id));
 
+  // append timestamp to video URL
+  let url = generatePath(linkTemplate, { videoId: id });
+  if (timestamp !== undefined) {
+    url += `?t=${timestamp}`;
+  }
+
   return (
     <Link
       key={id}
-      to={generatePath(linkTemplate, { videoId: id })}
+      to={url}
       onClick={(e) => {
         if (!openModal) return;
         e.preventDefault();
