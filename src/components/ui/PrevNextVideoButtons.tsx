eda3ebeb695178887769d631c9671a4ffb01f8e5
Horace Lee <horaceleedev@gmail.com>
2026-02-16 13:39:44 +0000

    Apply React.memo to prev/next navigation buttons
    Since the PrevNextVideoButtons component contains a findIndex operation which is somewhat computationally expensive, I think applying React.memo should help improve rendering performance

--- src/components/ui/PrevNextVideoButtons.tsx
+++ src/components/ui/PrevNextVideoButtons.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import { Video } from "../../types";
 import { generatePath, useNavigate } from "react-router-dom";
 import { Button, Flex } from "antd";
@@ -8,45 +8,47 @@ const PrevNextVideoButtons: FC<{
   video: Video,
   videoLinkTemplate?: string,
   videos?: Video[],
-}> = ({
-  video,
-  videoLinkTemplate = "/videos/:videoId",
-  videos = [],
-}) => {
-  const navigate = useNavigate();
-  const videoIndex = videos.findIndex(v => v.id === video.id);
-  let nextVideoId = '';
-  let prevVideoId = '';
-  if (videoIndex < videos.length - 1) {
-    // If not the last video
-    nextVideoId = videos[videoIndex + 1].id;
-  }
-  if (videoIndex > 0) {
-    // If not the first video
-    prevVideoId = videos[videoIndex - 1].id;
-  }
+}> = memo(
+  ({
+    video,
+    videoLinkTemplate = "/videos/:videoId",
+    videos = [],
+  }) => {
+    const navigate = useNavigate();
+    const videoIndex = videos.findIndex(v => v.id === video.id);
+    let nextVideoId = '';
+    let prevVideoId = '';
+    if (videoIndex < videos.length - 1) {
+      // If not the last video
+      nextVideoId = videos[videoIndex + 1].id;
+    }
+    if (videoIndex > 0) {
+      // If not the first video
+      prevVideoId = videos[videoIndex - 1].id;
+    }
 
-  function navigateToVideo(videoId: string) {
-    return () => navigate(generatePath(videoLinkTemplate, { videoId }));
-  }
+    function navigateToVideo(videoId: string) {
+      return () => navigate(generatePath(videoLinkTemplate, { videoId }));
+    }
 
-  return (
-    <Flex gap="middle" justify="start">
-      <Button
-        aria-label="Previous video"
-        shape="circle" icon={<LeftOutlined />}
-        disabled={!prevVideoId}
-        onClick={navigateToVideo(prevVideoId)}
-      />
-      <Button
-        aria-label="Next video"
-        shape="circle"
-        icon={<RightOutlined />}
-        disabled={!nextVideoId}
-        onClick={navigateToVideo(nextVideoId)}
-      />
-    </Flex>
-  );
-};
+    return (
+      <Flex gap="middle" justify="start">
+        <Button
+          aria-label="Previous video"
+          shape="circle" icon={<LeftOutlined />}
+          disabled={!prevVideoId}
+          onClick={navigateToVideo(prevVideoId)}
+        />
+        <Button
+          aria-label="Next video"
+          shape="circle"
+          icon={<RightOutlined />}
+          disabled={!nextVideoId}
+          onClick={navigateToVideo(nextVideoId)}
+        />
+      </Flex>
+    );
+  }
+);
 
 export default PrevNextVideoButtons;
