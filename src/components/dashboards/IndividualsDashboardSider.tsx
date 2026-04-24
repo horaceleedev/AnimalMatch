25d8bcfc2a9e68b360c38f063d69e495bd628af6
Horace Lee <horaceleedev@gmail.com>
2026-04-24 20:39:21 +0100

    Show age/sex menu items in IndividualsDashboardSider

--- src/components/dashboards/IndividualsDashboardSider.tsx
+++ src/components/dashboards/IndividualsDashboardSider.tsx
@@ -6,21 +6,21 @@ const { Sider } = Layout;
 import { MetadataFieldsType, UserRecord, Individual } from "../../types";
 import "./DashboardSider.scss";
 
-export const useIndividualsDashboardSiderState = (individuals: Individual[], _individualsMetadataFields: MetadataFieldsType, user: UserRecord | null) => {
+export const useIndividualsDashboardSiderState = (individuals: Individual[], individualsMetadataFields: MetadataFieldsType, user: UserRecord | null) => {
   const [selectedSiderKey, setSelectedSiderKey] = useState("all-individuals");
   const individualsBySiderKey: Record<string, Individual[]> = useMemo(() => ({
     "all-individuals": individuals,
     "created-by-me": user ? individuals.filter(individual => individual.created_by === user.id) : [],
-    // // ages
-    // ...individualsMetadataFields['age'].presetOptions!.reduce((acc: Record<string, Individual[]>, age: string) => {
-    //   acc['age/'+age] = individuals.filter(individual => individual.age === age);
-    //   return acc;
-    // }, {}),
-    // // sexes
-    // ...individualsMetadataFields['sex'].presetOptions!.reduce((acc: Record<string, Individual[]>, sex: string) => {
-    //   acc['sex/'+sex] = individuals.filter(individual => individual.sex === sex);
-    //   return acc;
-    // }, {}),
+    // ages
+    ...individualsMetadataFields['age'].presetOptions!.reduce((acc: Record<string, Individual[]>, age: string) => {
+      acc['age/'+age] = individuals.filter(individual => individual.age === age);
+      return acc;
+    }, {}),
+    // sexes
+    ...individualsMetadataFields['sex'].presetOptions!.reduce((acc: Record<string, Individual[]>, sex: string) => {
+      acc['sex/'+sex] = individuals.filter(individual => individual.sex === sex);
+      return acc;
+    }, {}),
     // custom tags
     ...individuals.reduce((acc: Record<string, Individual[]>, cur: Individual) => {
       for (const tag of cur.custom_tags) {
@@ -46,7 +46,7 @@ interface IndividualsDashboardSiderProps {
   uniqueValuesPerField: Record<string, string[]>;
 }
 export const IndividualsDashboardSider: FC<IndividualsDashboardSiderProps> = ({
-  selectedSiderKey, setSelectedSiderKey, individualsBySiderKey, individualsMetadataFields: _individualsMetadataFields, uniqueValuesPerField,
+  selectedSiderKey, setSelectedSiderKey, individualsBySiderKey, individualsMetadataFields, uniqueValuesPerField,
 }) => {
   return (
     <Sider className="dashboard-sider" style={{ /* background: colorBgContainer */ }} width={220}>
@@ -72,26 +72,26 @@ export const IndividualsDashboardSider: FC<IndividualsDashboardSiderProps> = ({
           //   key: 'recently-added',
           //   label: 'Recently added',
           // },
-          // {
-          //   key: 'by-age',
-          //   label: 'By age',
-          //   type: 'group',
-          //   children: individualsMetadataFields['age'].presetOptions!.map(age => ({
-          //     key: 'age/'+age,
-          //     label: <Typography.Text ellipsis={{tooltip: age}}>{age}</Typography.Text>,
-          //     extra: individualsBySiderKey['age/'+age].length,
-          //   })),
-          // },
-          // {
-          //   key: 'by-sex',
-          //   label: 'By sex',
-          //   type: 'group',
-          //   children: individualsMetadataFields['sex'].presetOptions!.map(sex => ({
-          //     key: 'sex/'+sex,
-          //     label: <Typography.Text ellipsis={{tooltip: sex}}>{sex}</Typography.Text>,
-          //     extra: individualsBySiderKey['sex/'+sex].length,
-          //   })),
-          // },
+          {
+            key: 'by-age',
+            label: 'By age',
+            icon: individualsMetadataFields['age'].icon,
+            children: individualsMetadataFields['age'].presetOptions!.map(age => ({
+              key: 'age/'+age,
+              label: age,
+              extra: individualsBySiderKey['age/'+age].length,
+            })),
+          },
+          {
+            key: 'by-sex',
+            label: 'By sex',
+            icon: individualsMetadataFields['sex'].icon,
+            children: individualsMetadataFields['sex'].presetOptions!.map(sex => ({
+              key: 'sex/'+sex,
+              label: sex,
+              extra: individualsBySiderKey['sex/'+sex].length,
+            })),
+          },
           (
             uniqueValuesPerField['custom_tags']?.length ?
             {
